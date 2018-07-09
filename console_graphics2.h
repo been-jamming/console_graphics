87c47dbba3b4fd55640632e09d87be163de1533b
Ben Jones <computergeek3.142@gmail.com>
2018-07-09 07:55:32 -0700

    Add files via upload

---
+++ console_graphics2.h
@@ -0,0 +1,61 @@
+#define true 1
+#define false 0
+
+typedef unsigned char bool;
+
+typedef struct color color;
+
+struct color{
+	unsigned char red;
+	unsigned char green;
+	unsigned char blue;
+};
+
+typedef struct variance variance;
+
+struct variance{
+	unsigned int num_elements;
+	double m;
+	double mean;
+};
+
+typedef struct screen screen;
+
+struct screen{
+	unsigned int size_x;
+	unsigned int size_y;
+	color **data;
+	char **chars;
+	color **foreground_colors;
+	color **background_colors;
+	double **best_variance;
+	variance **foreground_red[96];
+	variance **foreground_green[96];
+	variance **foreground_blue[96];
+	variance **background_red[96];
+	variance **background_green[96];
+	variance **background_blue[96];
+	char *print_str;
+};
+
+bool color_compare(color a, color b);
+
+double get_variance(variance *v);
+
+void add_value(variance *v, double value);
+
+void subtract_value(variance *v, double value);
+
+screen *create_screen(unsigned int size_x, unsigned int size_y);
+
+void recalculate_all(screen *s);
+
+void set_char(screen *s, unsigned int x, unsigned int y, char c, unsigned char foreground, unsigned char background);
+
+unsigned char match_color(color c);
+
+void set_pixel(screen *s, unsigned int x, unsigned int y, color c);
+
+void rect(screen *s, unsigned int x1, unsigned int y1, unsigned int x2, unsigned int y2, color c);
+
+void display(screen *s);
---
+++ displaybmp.c
@@ -0,0 +1,38 @@
+#include "console_graphics2.h"
+#include "qdbmp.h"
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+	color c;
+	unsigned int width, height;
+	unsigned int x, y;
+	BMP* bmp;
+	screen *s;
+
+	if(argc != 2){
+		fprintf(stderr, "Usage: %s <input file>\n", argv[0]);
+		return 0;
+	}
+
+	printf("reading bitmap...\n");
+	bmp = BMP_ReadFile(argv[1]);
+	BMP_CHECK_ERROR(stdout, -1);
+
+	width = BMP_GetWidth(bmp);
+	height = BMP_GetHeight(bmp);
+	
+	printf("loading...\n");
+	s = create_screen(width/8 + 1, height/13 + 1);
+	for(x = 0; x < width; x++){
+		for(y = 0; y < height; y++){
+			BMP_GetPixelRGB(bmp, x, y, &c.red, &c.green, &c.blue);
+			set_pixel(s, x, y, c);
+		}
+	}
+	display(s);
+	BMP_Free(bmp);
+	
+	printf("\e[0;0m");
+
+	return 0;
+}
